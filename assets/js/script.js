d832ac8882847d4d81348dde00bb124cd3640d7b
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-06-24 17:18:07 -0500

    added trash droppable

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,6 +138,70 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 })
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function(event){
+    var tempArr =[];
+    $(this).children().each(function(){
+
+
+      
+
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim()
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+  });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+    
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+}});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event,ui){
+    console.log("over");
+  },
+  out: function(event,ui) {
+    console.log("out");
+  }
+
+});
+
+
+
 // load tasks for the first time
 loadTasks();
 
