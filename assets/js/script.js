27ecbd2f66ce7492080793c015996b0a53d6cc52
Ruminert <ruminertt@Tanners-Air.attlocal.net>
2022-06-27 15:48:39 -0500

    added UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,7 +86,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -156,11 +156,19 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event){
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
+    $(event.target).addClass("dropover-active");
     console.log("over", event.target);
   },
   out: function (event) {
+    $(event.target).removeClass("dropover-active")
     console.log("out", event.target);
   },
   update: function(event){
@@ -199,13 +207,16 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui){
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("drop");
     ui.draggable.remove();
   },
   over: function(event,ui){
+    $(".bottom-trash").addClass("bottom-trash-active")
     console.log("over");
   },
   out: function(event,ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("out");
   }
 
@@ -238,8 +249,15 @@ var auditTask = function(taskEl){
  else if (Math.abs(moment().diff(time, "days"))<= 2){
    $(taskEl).addClass("list-group-item-warning");
  }
+ console.log(taskEl) 
  
 }
+
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  })
+}, 1800000);
 
 
 
